ae137c05cc0fdd86d7c4933bf9ed3ef0db3085dc
grtwje <63551230+grtwje@users.noreply.github.com>
2023-03-26 02:51:26 +0000

    cp

--- src/one_d_packed_array.rs
+++ src/one_d_packed_array.rs
@@ -23,15 +23,12 @@ impl OneDPackedArray {
         // * Loop through all rows containing one or more values.
         let mut it = TwoDArraySizeIterator::new(two_d_array);
         while let Some((row_index, row)) = it.next_biggest() {
-            println!("{row_index:?}: {row:?}");
-
             let col_indices = row.get_col_indices();
             if let Some(fist_col_index) = col_indices.first() {
                 if let Ok(rlt_seed) = isize::try_from(*fist_col_index) {
                     let first_array_unused_index =
                         *(unused_array_indices.first().unwrap()) as isize;
                     let mut rlt_value = -rlt_seed + first_array_unused_index;
-                    println!("rlt_value: {rlt_value}");
 
                     while _self.not_inserted(&mut unused_array_indices, row, rlt_value) {
                         rlt_value += 1; // todo step to next unused index
@@ -42,9 +39,6 @@ impl OneDPackedArray {
                         }
                     }
                     _self.rlt.insert(row_index, rlt_value);
-
-                    println!("new rlt_value: {rlt_value}");
-                    break;
                 } else {
                     return Err(Error::new(Kind::OneDPackedArrayError(
                         "Unexpected empty row found".to_string(),
@@ -71,24 +65,22 @@ impl OneDPackedArray {
 
         {
             let it = adj_col_indices.iter();
-            println!("..{adj_col_indices:?}");
             for i in it {
                 if !unused_array_indices.contains(i) {
-                    println!("{i} not in unused");
-                    println!("   {unused_array_indices:?}");
                     return true;
                 }
             }
         }
 
-        let _ = adj_col_indices
-            .iter()
-            .map(|i| unused_array_indices.remove(i));
+        let it = adj_col_indices.iter();
+        for i in it {
+            unused_array_indices.remove(i);
+        }
 
         let col_values = row.get_col_values();
         let it = zip(col_values.iter(), adj_col_indices.iter());
         for (v, adj_i) in it {
-            self.array.insert(*adj_i, *v);
+            self.array[*adj_i] = *v;
         }
 
         false
