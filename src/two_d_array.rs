709dbb18c01c9a6af4e9b5f5473ba4fcad4f09d5
grtwje <63551230+grtwje@users.noreply.github.com>
2023-04-15 14:19:06 -0400

    Added code comments.

--- src/two_d_array.rs
+++ src/two_d_array.rs
@@ -3,21 +3,55 @@ use std::cmp::Reverse;
 use std::collections::BTreeMap;
 use std::iter::zip;
 
+/// A 2D array that is used to store the indices of the words in the word list.
+/// The array is stored as a BTreeMap of rows. Each row is a BTreeMap of
+/// column indices.
 #[derive(Debug)]
 pub struct TwoDArray {
-    rows: BTreeMap<usize, Row>,
+    // The rows of the 2D array.
+    // The key is the row index and the value is the row.
+    rows: BTreeMap<usize, Row>, // row index, row
+
+    // The row indices sorted by the number of entries in the row.
     rows_by_size: Vec<usize>,
+
+    // The number of entries in the array.
     num_entries: usize,
+
+    // The number of rows in the array.
     num_rows: usize,
+
+    // The index of the last row in the array.
     last_row_index: usize,
 }
 
+/// A row in the 2D array.
 #[derive(Debug)]
 pub struct Row {
-    cols: BTreeMap<usize, usize>,
+    // The columns of the row.
+    // The key is the column index and the value is the index of the word in the
+    // word list.
+    cols: BTreeMap<usize, usize>, // col index, word index
 }
 
 impl<'a> TwoDArray {
+    /// Create a new 2D array.
+    /// The array is created by hashing each word in the word list and using the
+    /// hash values as the row and column indices.
+    /// The array is then sorted by the number of entries in each row.
+    ///
+    /// # Parameters
+    /// * `word_list` - The word list to create the array from.
+    /// * `hash_algorithm` - The hash algorithm to use.
+    ///
+    /// # Returns
+    /// A new 2D array.
+    ///
+    /// # Errors
+    /// Will return `Err` if there is a collision in the array.
+    /// A collision occurs when two words hash to the same row and column.
+    /// This will happen if the hash algorithm is not good enough. A new algorithm
+    /// will need to be tried.
     pub fn new(word_list: &WordList, hash_algorithm: &dyn HashAlgorithm) -> Result<Self, Error> {
         let num_words = word_list.len();
 
@@ -76,19 +110,43 @@ impl<'a> TwoDArray {
         Ok(self_)
     }
 
+    // Get the number of entries in the array.
+    //
+    // # Returns
+    // The number of entries in the array.
     pub fn get_num_entries(&self) -> usize {
         self.num_entries
     }
 
+    // Get the index of the last row in the array.
+    //
+    // # Returns
+    // The index of the last row in the array.
     pub fn get_last_row_index(&self) -> usize {
         self.last_row_index
     }
 
+    // Get the number of rows in the array.
+    // Only used for testing.
+    //
+    // # Returns
+    // The number of rows in the array.
     #[cfg(test)]
     pub fn get_num_rows(&self) -> usize {
         self.num_rows
     }
 
+    // Get a row of the array by its size.
+    //
+    // Index 0 is the row with the most entries.
+    // Index 1 is the row with the second most entries.
+    // etc.
+    //
+    // # Parameters
+    // * `index` - The index of the row to get.
+    //
+    // # Returns
+    // The row at the given index.
     fn get_row_by_size(&'a self, index: usize) -> Option<(usize, &'a Row)> {
         if let Some(size_index) = self.rows_by_size.get(index) {
             if let Some(row) = self.rows.get(size_index) {
@@ -100,24 +158,37 @@ impl<'a> TwoDArray {
 }
 
 impl Row {
+    // Get the used column indices in the row.
+    //
+    // # Returns
+    // The used column indices in the row.
     pub fn get_col_indices(&self) -> Vec<usize> {
         let indices: Vec<_> = self.cols.keys().copied().collect();
         indices
     }
 
+    // Get the column values in the row.
+    //
+    // # Returns
+    // The column values in the row.
     pub fn get_col_values(&self) -> Vec<usize> {
         let values: Vec<_> = self.cols.values().copied().collect();
         values
     }
 }
 
+// Iterator for the rows of a 2D array sorted by size.
 #[derive(Debug)]
 pub struct RowSizeIterator<'a> {
+    // The 2D array to iterate over.
     two_d_array: &'a TwoDArray,
+
+    // The current index of the iterator.
     index: usize,
 }
 
 impl<'a> RowSizeIterator<'a> {
+    // Create a new iterator.
     pub fn new(array: &'a TwoDArray) -> Self {
         RowSizeIterator {
             two_d_array: array,
@@ -125,6 +196,10 @@ impl<'a> RowSizeIterator<'a> {
         }
     }
 
+    // Get the next biggest row in the array.
+    //
+    // # Returns
+    // The next biggest row in the array.
     pub fn next_biggest(&mut self) -> Option<(usize, &'a Row)> {
         if let Some((index, row)) = self.two_d_array.get_row_by_size(self.index) {
             self.index += 1;
